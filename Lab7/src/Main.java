b0fc05240996c475badd1d8cf94b4e9f2600f139
PaulAdam <paul.adrian2001@yahoo.com>
2023-01-13 10:16:35 +0200

    Refactor Lab7

--- Lab7/src/Main.java
+++ Lab7/src/Main.java
@@ -1,26 +1,54 @@
 import mpi.MPI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
-    static final int POLYNOMIAL_SIZE = 5;
-    static final String IMPLEMENTATION = "regular";
+    static final int polynomialDegree = 80;
+    static final String algorithmType = "CLASSIC";  // KARATSUBA
 
-    public static void masterProcess(Polynomial p1, Polynomial p2, int processes) {
-        long startTime = System.nanoTime();
-        int length = p1.getSize() / (processes - 1);
+    public static void main(String[] args) {
+        MPI.Init(args);                                      // an MPI program must call MPI.Init in the beginning
 
-        // split the coefficients of the polynomials on processes
+        int idOfCurrentProcess = MPI.COMM_WORLD.Rank();     // one's own ID
+        int size = MPI.COMM_WORLD.Size();                   // the number of launched instances
 
-        int start;
-        int end = 0;
+        if (idOfCurrentProcess == 0){
+            System.out.println("Parent process: \n");
+            Polynomial p1 = new Polynomial(polynomialDegree);
+            Polynomial p2 = new Polynomial(polynomialDegree);
 
-        for (int i = 1; i < processes; i++) {
-            start = end;
-            end = start + length;
-            if (i == processes - 1) {
-                end = p1.getSize();         // last coefficient of the polynomial
+            System.out.println("Polynomial 1:" + p1);
+            System.out.println("Polynomial 2:" + p2);
+            System.out.println("\n");
+
+            parentTask(p1, p2, size);
+        } else {
+            if (Objects.equals(algorithmType, "CLASSIC")) {
+                childClassicTask(idOfCurrentProcess);
+            } else {
+                childKaratsubaTask(idOfCurrentProcess);
             }
+        }
+
+        MPI.Finalize();                                 // an MPI program must call MPI.Finalize in the end
+    }
+
+    public static void parentTask(Polynomial p1, Polynomial p2, int numberOfProcesses) {
+        Polynomial resultOfParentComputation;
+
+        // split tasks to children
+        int taskLength = (p1.getDegree() + 1) / numberOfProcesses;
+        int startCoefficientOfParent = 0;
+
+        // the parent will compute as many coefficients as the children (taskLength (-1 because we start from 0)) and any remainder of the split ((p1.getDegree() + 1) % numberOfProcesses)
+        int endCoefficientOfParent = taskLength - 1 + (p1.getDegree() + 1) % numberOfProcesses;
+
+        int endCoefficient = endCoefficientOfParent;
+        for(int i = 1; i < numberOfProcesses; i++) {              // send to children
+            int startCoefficient = endCoefficient + 1;
+            endCoefficient = endCoefficient + taskLength;
 
             // offset = from where we start (what we send)
             // count = how much we send
@@ -28,100 +56,89 @@ public class Main {
             // i = id of the process to which we send the information
             MPI.COMM_WORLD.Send(new Object[]{p1}, 0, 1, MPI.OBJECT, i, 0);
             MPI.COMM_WORLD.Send(new Object[]{p2}, 0, 1, MPI.OBJECT, i, 0);
-            MPI.COMM_WORLD.Send(new int[]{start}, 0, 1, MPI.INT, i, 0);
-            MPI.COMM_WORLD.Send(new int[]{end}, 0, 1, MPI.INT, i, 0);
+            MPI.COMM_WORLD.Send(new int[]{startCoefficient}, 0, 1, MPI.INT, i, 0);
+            MPI.COMM_WORLD.Send(new int[]{endCoefficient}, 0, 1, MPI.INT, i, 0);
         }
 
-        Object[] results = new Object[processes - 1];
-        for (int i = 1; i  < processes; i++){
-            MPI.COMM_WORLD.Recv(results, i - 1, 1, MPI.OBJECT, i, 0);
+        if(Objects.equals(algorithmType, "CLASSIC")) {              // here the parent does the work
+            System.out.println("Parent from " + startCoefficientOfParent + " to " + endCoefficientOfParent);
+
+            resultOfParentComputation = Polynomial.computeCoefficientsOfResult(p1, p2, startCoefficientOfParent, endCoefficientOfParent);
         }
-
-        Polynomial result = Algorithms.getResult(results);
-
-        long endTime = System.nanoTime();
-        double time = (endTime - startTime) / 1000000000.0;
-        System.out.println(IMPLEMENTATION + ":\n\n");
-        System.out.println("Execution time: " + time);
-        System.out.println("\n\n");
-        System.out.println(result + "\n\n");
-    }
-
-    public static void classicWorker(){
-        Object[] p1 = new Object[2];
-        Object[] p2= new Object[2];
-        int[] start = new int[1];
-        int[] end = new int[1];
-
-        // source = id of the process to which we send the information (here we send to process 0, the 'master' process)
-        MPI.COMM_WORLD.Recv(p1, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(p2, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(start, 0, 1, MPI.INT, 0, 0);
-        MPI.COMM_WORLD.Recv(end, 0, 1, MPI.INT, 0, 0);
-
-        Polynomial pol1 = (Polynomial) p1[0];
-        Polynomial pol2 = (Polynomial) p2[0];
-
-        Polynomial result = Algorithms.multiplySequence(pol1, pol2, start[0], end[0]);
-
-        MPI.COMM_WORLD.Send(new Object[]{result}, 0, 1, MPI.OBJECT, 0, 0);
-    }
-
-    public static void karatsubaWorker(){
-        Object[] p1 = new Object[2];
-        Object[] p2= new Object[2];
-        int[] start = new int[1];
-        int[] end = new int[1];
-
-        // source = id of the process to which we send the information (here we send to process 0, the 'master' process)
-        MPI.COMM_WORLD.Recv(p1, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(p2, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(start, 0, 1, MPI.INT, 0, 0);
-        MPI.COMM_WORLD.Recv(end, 0, 1, MPI.INT, 0, 0);
-
-        Polynomial pol1 = (Polynomial) p1[0];
-        Polynomial pol2 = (Polynomial) p2[0];
-
-        // we need the following 2 loops so that the process will know for what coefficients (from p1) to do the multiplications (with p2)
-        for (int i = 0; i < start[0]; i++) {
-            pol1.getCoefficients().set(i, 0);
-        }
-        for (int j = end[0]; j < pol1.getCoefficients().size(); j++) {
-            pol1.getCoefficients().set(j, 0);
-        }
-
-        Polynomial result = Algorithms.KaratsubaSequential(pol1, pol2);
-
-        MPI.COMM_WORLD.Send(new Object[]{result}, 0, 1, MPI.OBJECT, 0, 0);
-    }
-
-    public static void main(String[] args) {
-        MPI.Init(args);                     // an MPI program must call MPI.Init in the beginning
-
-        int me = MPI.COMM_WORLD.Rank();     // one's own ID
-        int size = MPI.COMM_WORLD.Size();   // the number of launched instances
-
-        if (me == 0){
-            System.out.println("Master process: \n");
-            Polynomial p1 = new Polynomial(POLYNOMIAL_SIZE);
-            p1.generateCoefficients();
-            Polynomial p2 = new Polynomial(POLYNOMIAL_SIZE);
-            p2.generateCoefficients();
-
-            System.out.println("Polynomial 1:" + p1);
-            System.out.println("Polynomial 2:" + p2);
-            System.out.println("\n\n");
-
-            masterProcess(p1, p2, size);
-        } else {
-            if (Objects.equals(IMPLEMENTATION, "regular")){
-                classicWorker();
+        else {
+            for (int j = endCoefficientOfParent; j < p1.getCoefficients().size(); j++) {        // the parent starts from 0, so we make the coefficients 0 only after the end
+                p1.getCoefficients().set(j, 0);
             }
-            if (Objects.equals(IMPLEMENTATION, "karatsuba")){
-                karatsubaWorker();
-            }
+
+            System.out.println("Parent from " + startCoefficientOfParent + " to " + endCoefficientOfParent);
+
+            resultOfParentComputation = Polynomial.KaratsubaSequential(p1, p2);
         }
 
-        MPI.Finalize();                     // an MPI program must call MPI.Finalize in the end
+        System.out.println("Parent done.");
+
+        Object[] results = new Object[numberOfProcesses-1];
+        for (int i = 1; i  < numberOfProcesses; i++){
+            // source = id of the process from which we receive the information
+            MPI.COMM_WORLD.Recv(results, i-1, 1, MPI.OBJECT, i, 0);
+        }
+
+        ArrayList<Object> resultsAsArray = new ArrayList<>(Arrays.asList(results));
+        resultsAsArray.add(resultOfParentComputation);
+
+        Polynomial finalResult = Polynomial.computeSumOfPolynomials(resultsAsArray);
+
+        System.out.println("Final result: " + finalResult);
+    }
+
+    public static void childClassicTask(int id) {
+        Object[] p1Object = new Object[2];
+        Object[] p2Object= new Object[2];
+        int[] startCoefficient = new int[1];
+        int[] endCoefficient = new int[1];
+
+        MPI.COMM_WORLD.Recv(p1Object, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(p2Object, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(startCoefficient, 0, 1, MPI.INT, 0, 0);
+        MPI.COMM_WORLD.Recv(endCoefficient, 0, 1, MPI.INT, 0, 0);
+
+        Polynomial p1 = (Polynomial) p1Object[0];
+        Polynomial p2 = (Polynomial) p2Object[0];
+
+        System.out.println("Child with id " + id + " from " + startCoefficient[0] + " to " + endCoefficient[0]);
+
+        Polynomial result = Polynomial.computeCoefficientsOfResult(p1, p2, startCoefficient[0], endCoefficient[0]);
+        MPI.COMM_WORLD.Send(new Object[]{result}, 0, 1, MPI.OBJECT, 0, 0);
+
+        System.out.println("Child with id " + id + " done.");
+    }
+
+    public static void childKaratsubaTask(int id) {
+        Object[] p1Object = new Object[2];
+        Object[] p2Object= new Object[2];
+        int[] startCoefficient = new int[1];
+        int[] endCoefficient = new int[1];
+
+        MPI.COMM_WORLD.Recv(p1Object, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(p2Object, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(startCoefficient, 0, 1, MPI.INT, 0, 0);
+        MPI.COMM_WORLD.Recv(endCoefficient, 0, 1, MPI.INT, 0, 0);
+
+        Polynomial p1 = (Polynomial) p1Object[0];
+        Polynomial p2 = (Polynomial) p2Object[0];
+
+        System.out.println("Child with id " + id + " from " + startCoefficient[0] + " to " + endCoefficient[0]);
+
+        for (int i = 0; i < startCoefficient[0]; i++) {     // set the coefficients before start to 0 in order to make the right calculations
+            p1.getCoefficients().set(i, 0);
+        }
+        for (int j = endCoefficient[0] + 1; j < p1.getCoefficients().size(); j++) {     // set the coefficients after end to 0 in order to make the right calculations
+            p1.getCoefficients().set(j, 0);
+        }
+
+        Polynomial result = Polynomial.KaratsubaSequential(p1, p2);
+        MPI.COMM_WORLD.Send(new Object[]{result}, 0, 1, MPI.OBJECT, 0, 0);
+
+        System.out.println("Child with id " + id + " done " + result);
     }
 }
--- Lab7/src/Polynomial.java
+++ Lab7/src/Polynomial.java
@@ -2,55 +2,91 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Polynomial implements Serializable {
-    public final List<Integer> coefficients;
+    private List<Integer> coefficients;
+    private int degree;
+
+    public Polynomial(int degree) {
+        this.degree = degree;
+
+        // generate coefficients
+        this.coefficients = new ArrayList<>();
+        Random randomGenerator = new Random();
+        for(int i = 0; i < degree; i++) {
+            coefficients.add(randomGenerator.nextInt(10));
+        }
+        // the free term
+        coefficients.add(randomGenerator.nextInt(10));
+    }
 
     public Polynomial(List<Integer> coefficients) {
         this.coefficients = coefficients;
-    }
-
-    public Polynomial(int size) {
-        this.coefficients = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            coefficients.add(0);
-        }
-    }
-
-    public void generateCoefficients() {
-        Random random = new Random();
-        this.coefficients.replaceAll(ignored -> random.nextInt(10));
-    }
-
-    public int getDegree() {
-        return this.coefficients.size() - 1;
+        this.degree = coefficients.size() - 1;
     }
 
     public List<Integer> getCoefficients() {
-        return this.coefficients;
+        return coefficients;
     }
 
-    public int getSize() {
-        return this.coefficients.size();
+    public int getDegree() {
+        return degree;
     }
 
-    public static Polynomial add(Polynomial first, Polynomial second) {
-        int minDegree = Math.min(first.getDegree(), second.getDegree());
-        int maxDegree = Math.max(first.getDegree(), second.getDegree());
+    // A polynomial with 0 values in the array of coefficients
+    public static Polynomial EmptyPolynomial(int degree) {
+        // generate coefficients
+        List<Integer> zeroCoefficients = new ArrayList<>();
+        for(int i = 0; i <= degree; i++) {
+            zeroCoefficients.add(0);
+        }
+        return new Polynomial(zeroCoefficients);
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        if (coefficients.get(degree) != 0) {
+            if(coefficients.get(degree) != 1) {
+                builder.append(coefficients.get(degree)).append("x^").append(degree);
+            } else {
+                builder.append("x^").append(degree);
+            }
+        }
+
+        for (var i = degree - 1; i > 0; --i) {
+            if (coefficients.get(i) != 0) {
+                if(coefficients.get(i) != 1) {
+                    builder.append(" + ").append(coefficients.get(i)).append("x^").append(i);
+                } else {
+                    builder.append(" + ").append("x^").append(i);
+                }
+            }
+        }
+        if (coefficients.get(0) != 0)
+            builder.append(" + ").append(coefficients.get(0));
+        return builder.toString();
+    }
+
+    public static Polynomial add(Polynomial p1, Polynomial p2){
+        int minDegree = Math.min(p1.getDegree(), p2.getDegree());
+        int maxDegree = Math.max(p1.getDegree(), p2.getDegree());
         List<Integer> coefficients = new ArrayList<>(maxDegree + 1);
 
+        //Add the 2 polynomials
         for (int i = 0; i <= minDegree; i++) {
-            coefficients.add(first.coefficients.get(i) + second.coefficients.get(i));
+            coefficients.add(p1.getCoefficients().get(i) + p2.getCoefficients().get(i));
         }
 
         if (minDegree != maxDegree) {
-            if (maxDegree == first.getDegree()) {
+            if (maxDegree == p1.getDegree()) {
                 for (int i = minDegree + 1; i <= maxDegree; i++) {
-                    coefficients.add(first.coefficients.get(i));
+                    coefficients.add(p1.getCoefficients().get(i));
                 }
             } else {
                 for (int i = minDegree + 1; i <= maxDegree; i++) {
-                    coefficients.add(second.coefficients.get(i));
+                    coefficients.add(p2.getCoefficients().get(i));
                 }
             }
         }
@@ -58,23 +94,23 @@ public class Polynomial implements Serializable {
         return new Polynomial(coefficients);
     }
 
-    public static Polynomial subtract(Polynomial first, Polynomial second) {
-        int minDegree = Math.min(first.getDegree(), second.getDegree());
-        int maxDegree = Math.max(first.getDegree(), second.getDegree());
+    public static Polynomial subtract(Polynomial p1, Polynomial p2){
+        int minDegree = Math.min(p1.getDegree(), p2.getDegree());
+        int maxDegree = Math.max(p1.getDegree(), p2.getDegree());
         List<Integer> coefficients = new ArrayList<>(maxDegree + 1);
 
         for (int i = 0; i <= minDegree; i++) {
-            coefficients.add(first.coefficients.get(i) - second.coefficients.get(i));
+            coefficients.add(p1.getCoefficients().get(i) - p2.getCoefficients().get(i));
         }
 
         if (minDegree != maxDegree) {
-            if (maxDegree == first.getDegree()) {
+            if (maxDegree == p1.getDegree()) {
                 for (int i = minDegree + 1; i <= maxDegree; i++) {
-                    coefficients.add(first.coefficients.get(i));
+                    coefficients.add(p1.getCoefficients().get(i));
                 }
             } else {
                 for (int i = minDegree + 1; i <= maxDegree; i++) {
-                    coefficients.add(second.coefficients.get(i));
+                    coefficients.add(p2.getCoefficients().get(i));
                 }
             }
         }
@@ -88,25 +124,67 @@ public class Polynomial implements Serializable {
         return new Polynomial(coefficients);
     }
 
-    public static Polynomial addZeroes(Polynomial polynomial, int offset) {
-        List<Integer> coefficients = new ArrayList<>();
-        for (int i = 0; i < offset; i++) {
-            coefficients.add(0);
-        }
-        coefficients.addAll(polynomial.coefficients);
-
+    public static Polynomial addZeros(Polynomial p, int offset) {
+        List<Integer> coefficients = IntStream.range(0, offset).mapToObj(i -> 0).collect(Collectors.toList());
+        coefficients.addAll(p.getCoefficients());
         return new Polynomial(coefficients);
     }
 
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
+    public static Polynomial computeCoefficientsOfResult(Polynomial p1, Polynomial p2, int startCoefficient, int endCoefficient) {
+        Polynomial result = EmptyPolynomial(2 * p1.getDegree());
 
-        for (int i = this.coefficients.size() - 1; i >= 0; i--) {
-            stringBuilder.append(this.coefficients.get(i)).append("x^").append(i).append(" + ");
+        for (int i = startCoefficient; i <= endCoefficient; i++) {
+            for (int j = 0; j < p2.getCoefficients().size(); j++) {
+                result.getCoefficients().set(i + j, result.getCoefficients().get(i + j) + p1.getCoefficients().get(i) * p2.getCoefficients().get(j));
+            }
         }
-        stringBuilder.setLength(stringBuilder.length() - 3);
 
-        return stringBuilder.toString();
+        return result;
+    }
+
+    public static Polynomial computeSumOfPolynomials(ArrayList<Object> polynomials) {
+        int degree = ((Polynomial) polynomials.get(0)).getDegree();
+        Polynomial result = EmptyPolynomial(degree);
+
+        for (Object polynomial: polynomials) {
+            result = Polynomial.add(result, (Polynomial) polynomial);
+        }
+
+        return result;
+    }
+
+    public static Polynomial regularSequential(Polynomial p1, Polynomial p2){
+        Polynomial result = EmptyPolynomial(p1.getDegree() + p2.getDegree());
+
+        for (int i = 0; i < p1.getCoefficients().size(); i++) {
+            for (int j = 0; j < p2.getCoefficients().size(); j++) {
+                int index = i + j;
+                int rez = p1.getCoefficients().get(i) * p2.getCoefficients().get(j) + result.getCoefficients().get(index);
+                result.getCoefficients().set(index, rez);
+            }
+        }
+
+        return  result;
+    }
+
+    public static Polynomial KaratsubaSequential(Polynomial p1, Polynomial p2){
+        if (p1.getDegree() < 2 || p2.getDegree() < 2)
+            return regularSequential(p1, p2);
+
+        int m = Math.max(p1.getDegree(), p2.getDegree()) / 2;
+
+        Polynomial low1 = new Polynomial(p1.getCoefficients().subList(0, m));
+        Polynomial high1 = new Polynomial(p1.getCoefficients().subList(m, p1.getDegree() + 1));
+        Polynomial low2 = new Polynomial(p2.getCoefficients().subList(0, m));
+        Polynomial high2 = new Polynomial(p2.getCoefficients().subList(m, p2.getDegree() + 1));
+
+        Polynomial z0 = KaratsubaSequential(low1, low2);
+        Polynomial z1 = KaratsubaSequential(Polynomial.add(low1, high1), Polynomial.add(low2, high2));
+        Polynomial z2 = KaratsubaSequential(high1, high2);
+
+        Polynomial rez1 = Polynomial.addZeros(z2, 2 * m);
+        Polynomial rez2 = Polynomial.addZeros(Polynomial.subtract(Polynomial.subtract(z1, z2), z0), m);
+
+        return Polynomial.add(Polynomial.add(rez1, rez2), z0);
     }
 }
